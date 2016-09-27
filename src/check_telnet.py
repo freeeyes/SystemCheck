178354bfbb830055a290f00584b8e5565f7a6e14
freeeyes <freeeyes@163.com>
2016-09-27 15:07:00 +0800

    完善监听检测代码

--- src/check_telnet.py
+++ src/check_telnet.py
@@ -15,7 +15,7 @@ def L_Telnet(strHostIP, strPort):
 		return strText
 	list = RetList[1].split()
 	#print "]L_Telnet]list=" + str(len(list))
-	if(len(list) != 6):
+	if(len(list) < 6):
 		strText = "链接状态不正常(" + strHostIP + ":" + strPort + ")"
 		return strText		
 		
@@ -33,19 +33,23 @@ def L_Telnet_Listen(strPort):
 		strText = "没有找到监听端口(" + strPort + ")"
 		return strText
 	list = RetList[1].split()
-	print "]L_Telnet_Listen]list=" + str(len(list))
-	if(len(list) != 6):
-		strText = "监听状态不正常(" + strPort + ")"
+	#print "[L_Telnet_Listen]list=" + str(len(list))
+	#print "[L_Telnet_Listen]list[5]=" + str(list[5])
+	if(len(list) < 6):
+		strText = "[error]监听状态不正常(" + strPort + ")"
 		return strText		
-		
+	
 	if list[5].strip() == "LISTEN":
 		strText = "监听端口(" + strPort + ") 状态为[正常]" 
 	else:
 		strText = "[error]监听端口(" + strPort + ") 状态为[" + list[5].strip() + "]" 
 	return strText	
-	
-'''	
+
+'''		
 #测试代码
 if __name__ == "__main__":
-	print L_Telnet("124.117.209.132", "10050")
-'''	
+	#print L_Telnet("124.117.209.132", "10050")
+	
+	print L_Telnet_Listen("8010")
+'''
+	
