41addc84df3a42469a9bb671a8f322511d52336c
freeeyes <freeeyes@163.com>
2016-11-25 11:12:12 +0800

    重写检测Linux自由内存剩余的脚本

--- src/check_memory.py
+++ src/check_memory.py
@@ -1,26 +1,29 @@
-#!/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import os, sys, string
-import commands 
+import commands
 
 #add by freeeyes
 
 #获得当前空余内存的存量(参数单位是K)
 def L_FreeMemory(nWarningSize):
-	strText = ""
-	strCommandLine  = "free | grep 'Mem:'"
-	strCurrFreeMemory = os.popen(strCommandLine).read()
-	#print("[L_FreeMemory]Free memory=%d" %(int(strCurrFreeMemory)))
-	strMenList = strCurrFreeMemory.split()
-	fMemoryUser = float(float(strMenList[2]) - float(strMenList[6]))/float(strMenList[1]) * float(100.0)
-	strText = "所有内存: " + strMenList[1]  + ",使用内存: " + strMenList[2] + ",自由内存: " + strMenList[3] + ",使用率： " + str(fMemoryUser) +"%"
-	if((int(strMenList[3]) + int(strMenList[6])) < nWarningSize):
-		return True,strText
-	else:
-		return False,strText
-		
-#测试代码		
-#if __name__ == "__main__": 
-#	 if(True == L_FreeMemory(200000)):
-#		print "[L_FreeMemory]Free Memory is more"
-		
+        strText = ""
+        strCommandLine  = "free | grep 'Mem:'"
+        strCurrFreeMemory = os.popen(strCommandLine).read()
+        #print("[L_FreeMemory]Free memory=%d" %(int(strCurrFreeMemory)))
+        strMenList = strCurrFreeMemory.split()
+        fMemoryUser = (float(strMenList[2]) - float(strMenList[6]))/float(strMenList[1]) * float(100.0)
+
+        #print("[L_FreeMemory]all=%f" %(float(strMenList[1])))
+        #print("[L_FreeMemory]Rote=%f" %(fMemoryUser))
+        nCurrFreeMemory = int(strMenList[1]) - (int(strMenList[2]) - int(strMenList[6]))
+        strText = "所有内存: " + strMenList[1]  + ",使用内存: " + str(int(strMenList[2]) - int(strMenList[6])) + ",自由内存: " + str(nCurrFreeMemory) + ",使用率： " + str(fMemoryUser) +"%"
+        #print("[L_FreeMemory]strText=%s"%(strText))
+        if(nCurrFreeMemory < nWarningSize):
+                return True,strText
+        else:
+                return False,strText
+
+#测试代码
+#if __name__ == "__main__":
+#        if(True == L_FreeMemory(1*1024*1024)):
+#               print "[L_FreeMemory]Free Memory is more"
